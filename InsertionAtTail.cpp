a487039a566938e78d77b23ee20b967a1d06a42a
rajnish <rajnishprasadkalwar@gmail.com>
2022-12-29 13:16:53 +0000

    INsertion at end

---
+++ InsertionAtTail.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    //constructor
+    Node(int data)
+    {
+        this->data=data;
+        this->next=NULL;
+
+    }
+
+};
+//to insert the node
+/*void insertAtHead(Node* &head,int d){
+    Node* temp=new Node(d);
+    temp->next=head;
+    head=temp;
+
+}*/
+//to insert node at tail
+void insertAtTail(Node* &tail,int d){
+    Node* temp=new Node(d);
+    tail->next=temp;
+    tail=tail->next;
+}
+
+// to  print or transverse the node
+void print(Node* head){
+Node* temp=head;
+
+    while(temp!=NULL){
+       cout<<temp->data<<" ";
+       temp=temp->next;
+} 
+cout<<endl;
+ 
+}
+int main(){
+    Node* node1=new Node(10);
+    Node* head=node1;
+    Node* tail=node1;
+    print(head);
+    insertAtTail(tail,12);
+    print(head);
+    insertAtTail(tail,15);
+    print(head);
+
+
+}
+
+
